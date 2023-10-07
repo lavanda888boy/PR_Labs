e079070e1b88f0a63c37599dbac234c220472a59
Sevastian Bajenov <sevabaj@gmail.com>
2023-10-07 20:37:41 +0300

    Add json protocol for user connection.

--- Lab_5/client.py
+++ Lab_5/client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json
 
 HOST = '127.0.0.1' 
 PORT = 8080
@@ -8,29 +9,47 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 client_socket.connect((HOST, PORT))
+
+name = input('Introduce your name: ')
+room = input('Introduce the room you would like to join: ')
+connection_message = {
+                        "type": "connect",
+                        "payload": {
+                            "name": name,
+                            "room": room
+                        }
+                    }   
+data = json.dumps(connection_message)
+client_socket.sendall(bytes(data, encoding='utf-8'))
+
 print(f"Connected to {HOST}:{PORT}")
 
 
 def receive_messages():
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-        
         if not message:
             break 
+
+        data = json.loads(message)
+        if data['type'] == 'connect_ack':
+            print(data['payload']['message'])
+
+
+def main():
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.daemon = True
+    receive_thread.start()
+
+    while True:
+        message = input("Enter a message (or 'exit' to quit): ")
+        if message.lower() == 'exit':
+            break
         
-        print(f"Received: {message}")
+        client_socket.send(message.encode('utf-8'))
+
+    client_socket.close()
 
 
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.daemon = True
-receive_thread.start()
-
-while True:
-    message = input("Enter a message (or 'exit' to quit): ")
-    
-    if message.lower() == 'exit':
-        break
-    
-    client_socket.send(message.encode('utf-8'))
-
-client_socket.close()
+if __name__ == '__main__':
+    main()
--- Lab_5/server.py
+++ Lab_5/server.py
@@ -1,37 +1,56 @@
 import socket
 import threading
+import json
 
 HOST = '127.0.0.1' 
 PORT = 8080
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 
 server_socket.listen()
-print(f"Server is listening on {HOST}:{PORT}")
+print(f'Server is listening on {HOST}:{PORT}')
 
 
-def handle_client(client_socket, client_address):
-    print(f"Accepted connection from {client_address}")
+def handle_client(client_socket, client_address, clients):
+    print(f'Accepted connection from {client_address}')
+
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-
         if not message:
             break
 
-        print(f"Received from {client_address}: {message}")
-        
+        data = json.loads(message)
+        if data['type'] == 'connect':
+            acknowledge_message = {
+                                    "type": "connect_ack",
+                                    "payload": {
+                                        "message": f"\nYou succcesfully connected to the room '{data['payload']['room']}'."
+                                    }
+                                }
+            server_data = json.dumps(acknowledge_message)
+            client_socket.send(bytes(server_data, encoding='utf-8'))
+
+        print(f'Received from {client_address}: {message}')
+        '''
         for client in clients:
             if client != client_socket:
                 client.send(message.encode('utf-8'))
+        '''
 
     clients.remove(client_socket)
     client_socket.close()
 
 
-clients = []
-while True:
-    client_socket, client_address = server_socket.accept()
-    clients.append(client_socket)
-    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-    client_thread.start()
+def main():
+    clients = []
+    while True:
+        client_socket, client_address = server_socket.accept()
+        clients.append(client_socket)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, clients))
+        client_thread.start()
+
+
+if __name__ == '__main__':
+    main()
