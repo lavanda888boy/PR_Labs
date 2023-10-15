679450a5f086428b483185a1d7da23e245c5fd9c
Sevastian Bajenov <sevabaj@gmail.com>
2023-10-15 19:00:47 +0300

    Add support for postgres db engine.

--- Lab_6/app.py
+++ Lab_6/app.py
@@ -6,7 +6,9 @@ from models.scooter import ElectroScooter
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scooter_database.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scooter_database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost:5432/scooter_db'
+
     db.init_app(app)
     
     return app
