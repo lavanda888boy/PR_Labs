f5c65141f2be27868930144a0c703c3d28efc20e
Sevastian Bajenov <sevabaj@gmail.com>
2023-10-26 11:48:32 +0300

    Conect consumer to the producer using ack messages.

--- Lab_7/consumer.py
+++ Lab_7/consumer.py
@@ -76,24 +76,29 @@ def findContacts(parser, advertisement_data):
 
 
 def main():
+    QUEUE = 'crawl'
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=QUEUE, durable=True)
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=QUEUE, on_message_callback=callback)
+    channel.basic_qos(prefetch_count=1)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print('Consumer interrupted')
         try:
             sys.exit(0)
         except SystemExit:
--- Lab_7/producer.py
+++ Lab_7/producer.py
@@ -4,8 +4,17 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+QUEUE = 'crawl'
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+channel.queue_declare(queue=QUEUE, durable=True)
+
+site_url = 'https://999.md/ro/list/real-estate/apartments-and-rooms?applied=1&eo=12900&eo=12912&eo=12885&eo=13859&ef=32&ef=33&o_33_1=776&page=10'
+channel.basic_publish(exchange='',
+                      routing_key=QUEUE,
+                      body=site_url,
+                      properties=pika.BasicProperties(
+                        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+                    ))
+
+print(f' [x] Sent initial url: {site_url}')
 connection.close()
